1fc9e2f71173ee4e53a64942fecaccf13b2f49c0
awhitbeck <awhitbe1@pc23.home>
2015-11-14 14:16:24 +0100

    adding my implementation of the matrix inversion caching functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+######################################
+######################################
+# This file implements functions to facilitate cache of inverted matrices.
+# There is one function, makeCacheMatrix, that will return a list of functions 
+# for storing and getting the target matrix and the inverted target matrix.  
+# There is one function, cacheSolve, which will use the standard R function, 
+# solve, to invert the matrix if the inverted matrix is not already cached.
+#
+#Author: Andrew Whitbeck - November 14, 2015
+#
+######################################
+######################################
 
-## Write a short comment describing this function
 
+#=====================================
+# makeCacheMatrix - 
+#-------------------------------------
+# stores a matrix and implements four functions for facilitating caching of
+# the inverted of the matrix.
+# 
+# Functions:
+# - - - - - - - - - - - - - - - - - - 
+# set(matrix) : stores matrix at m in functions environment 
+# get() : returns m that is intended to be inverted
+# getInverted(matrixInverted) : returns m^-1
+# setInverted() : saves m^-1 as mInv in the functions environment
 makeCacheMatrix <- function(x = matrix()) {
+  
+  mInv<-NULL 
+  
+  set <- function( yMatrix ){
+    x <<- yMatrix
+    mInv <<- NULL
+  }
+  get <- function() x
+  setInverted <- function(matrixInverted) mInv <<- matrixInverted
+  getInverted <- function() mInv
+  
+  list( set = set ,
+        get = get , 
+        getInverted = getInverted , 
+        setInverted = setInverted )
 
 }
 
-
-## Write a short comment describing this function
-
+#=====================================
+# cacheSolve - 
+#-------------------------------------
+# takes a 'makeCachMatrix', inverts it if the cache is empty and caches the 
+# result.  If the cache is not empty, it simply returns the cached mInv.
+# 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  if( is.null(matrixCache$getInverted()) ){
+    mInv <- solve( matrixCache$get() , ... )
+    matrixCache$setInverted( mInv )
+  }
+  
+  matrixCache$getInverted()
+
 }
